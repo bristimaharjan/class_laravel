0e3c0de458fa43ab91ce2adb8b24f33521730d78
bristimaharjan <bristimah@gmail.com>
2025-08-31 08:50:33 +0545

    day6-console

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -7,14 +7,15 @@
      @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
 <body>
+</html>
+<div class="max-w-2xl mx-auto p-4">
+<h1 class="text-2xl font-bold">Chirps</h1>
+<form  class = "space-y-4" action=" " method="post">
+    @csrf
+    <textarea  class="border border-gray-300 rounded-md p-2 w-full" name="content" rows="3" placeholder="What's happening" id=""></textarea>
+    <button type="submit" class="bg-blue-500 text-white  rounded-md  px-4 py-2 ">Chirps</button>
+</form>
+</div>
     
 </body>
 </html>
-<div>
-<h1>Chirps</h1>
-<form action=" " method="post">
-    @csrf
-    <textarea name="content" rows="3" placeholder="What's happening" id=""></textarea>
-    <button type="submit">Chirps</button>
-</form>
-</div>
